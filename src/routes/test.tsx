79181ee5b3b78e0732cf0d76ad0922a82a26c259
Ahmed Rizwan <ahmedrizwanextra@gmail.com>
2026-07-24 00:59:52 +0500

    Create test.tsx

---
+++ src/routes/test.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/test")({
+  component: TestComponent,
+});
+
+function TestComponent() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Test Page Works!</h1>
+    </div>
+  );
+}
