c469eb3b885d7a5c3090ddc723f8bf49c6ae7ca2
Ahmed Rizwan <ahmedrizwanextra@gmail.com>
2026-07-23 09:58:27 +0500

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL_HERE';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const supabaseUrl = 'https://owkxtvzxkxhehmlockis.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93a3h0dnp4a3hoZWhtbG9ja2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxOTg1NTcsImV4cCI6MjA5OTc3NDU1N30.Yk-jq-xD8XBWTf5DlNAO7s4IKDUDraoZcCMsi3obKCI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
